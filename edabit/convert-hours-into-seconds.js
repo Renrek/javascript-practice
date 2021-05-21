1dab162e06d8aded7830bbfd01161ae0e2989645
Brad Johansen <brad.d.johansen@gmail.com>
2021-05-21 10:28:06 -0500

    added a couple more

---
+++ edabit/convert-hours-into-seconds.js
@@ -0,0 +1,23 @@
+/*
+Convert Hours into Seconds
+Write a function that converts hours into seconds.
+
+Examples
+howManySeconds(2) ➞ 7200
+
+howManySeconds(10) ➞ 36000
+
+howManySeconds(24) ➞ 86400
+Notes
+60 seconds in a minute, 60 minutes in an hour
+Don't forget to return your answer.
+*/
+function howManySeconds(hours) {
+	return hours * Math.pow(60, 2)
+}
+/*
+Test.assertEquals(howManySeconds(2), 7200)
+Test.assertEquals(howManySeconds(10), 36000)
+Test.assertEquals(howManySeconds(24), 86400)
+Test.assertEquals(howManySeconds(36), 129600)
+*/
